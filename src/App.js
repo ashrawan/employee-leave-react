e748fb485c4e938bd415f2fe046705498bc6301d
ashrawan <shrawan.adhikari.31@gmail.com>
2018-09-27 11:50:14 +0545

    refactor to index.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-import AppRouter from './routers/AppRouter';
-
-import configureStore from './redux/store/configureStore';
-import { Provider } from 'react-redux';
-import axios from 'axios';
-const store = configureStore();
-
-// axios.defaults.baseURL = 'http://127.0.0.1:8080/api/';
-axios.defaults.baseURL = 'https://employeeleave.herokuapp.com/api/';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Provider store={store}>
-            <AppRouter />
-        </Provider>
+      
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './App.css';
+import AppRouter from './routers/AppRouter';
+
+import configureStore from './redux/store/configureStore';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+const store = configureStore();
+
+// axios.defaults.baseURL = 'http://127.0.0.1:8080/api/';
+axios.defaults.baseURL = 'https://employeeleave.herokuapp.com/api/';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
